fd86829f3e6d63210548fe276b003a375b0fa28e
Wouter Naessens <wouter.naessens@sterima.be>
2023-06-21 13:17:25 +0200

    Make both Lens and Prism more flexible in their uses

--- src/Refract/Lens.cs
+++ src/Refract/Lens.cs
@@ -7,7 +7,7 @@
 /// <typeparam name="TU">The projected type.</typeparam>
 public readonly struct Lens<T, TU> {
     readonly Func<T, TU> _get;
-    readonly Action<T, TU>? _set;
+    readonly Action<T, TU> _set;
 
     /// <summary>
     /// Initializes a new <see cref="Lens{T,TU}"/>.
@@ -16,11 +16,40 @@ public readonly struct Lens<T, TU> {
     /// <param name="set">An optional function to write a <typeparamref name="T"/>.</param>
     public Lens(Func<T, TU> get, Action<T, TU>? set = null) {
         _get = get;
-        _set = set;
+        _set = set ?? ((_, _) => throw new InvalidOperationException($"Cannot write a {typeof(TU)} to a {typeof(T)} using a readonly lens.")) ;
     }
 
+    /// <summary>
+    /// Reads from the given <paramref name="subject"/>.
+    /// </summary>
+    /// <param name="subject">The source object to read from.</param>
+    /// <returns>A <typeparamref name="TU?"/>.</returns>
+    public TU? Get(T subject) => _get(subject);
+
+    /// <summary>
+    /// Writes to the given <paramref name="subject"/>.
+    /// </summary>
+    /// <param name="subject">The object to write to.</param>
+    /// <param name="value">The value to write.</param>
+    public void Set(T subject, TU value) => _set.Invoke(subject, value);
+
+    /// <summary>
+    /// Deconstructs a <see cref="Lens{T,TU}"/> to its getter and setter functions respectively.
+    /// </summary>
+    /// <param name="get">The getter function to deconstruct to.</param>
+    /// <param name="set">The setter function to deconstruct to.</param>
+    public void Deconstruct(out Func<T, TU?> get, out Action<T, TU> set) => (get, set) = (_get, _set);
+
+    /// <summary>
+    /// Implicitly casts the given <see cref="Lens{T,TU}"/> to a <see cref="Func{TResult}"/> that reads a <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="instance">The <see cref="Lens{T,TU}"/> to cast.</param>
     public static implicit operator Func<T, TU>(Lens<T, TU> instance) => instance._get;
 
+    /// <summary>
+    /// Implicitly casts the given <see cref="Lens{T,TU}"/> to a <see cref="Action{T}"/> that writes to a <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="instance">The <see cref="Lens{T,TU}"/> to cast.</param>
     public static implicit operator Action<T, TU>(Lens<T, TU> instance) => instance._set ?? ((_, _) => { });
 }
 
