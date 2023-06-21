4b829ec47e1b542c573010e1dd105ac3caf9a209
Wouter Naessens <wouter.naessens@sterima.be>
2023-06-21 15:32:54 +0200

    Add selector lens for array

--- src/Refract/Optics/Array.cs
+++ src/Refract/Optics/Array.cs
@@ -24,4 +24,13 @@ public static class Array {
     /// <param name="lens">The <see cref="Lens{T,TU}"/> that produces the <see cref="IEnumerable{T}"/>.</param>
     /// <returns>A <see cref="Lens{T,TU}"/> that produces the first element in an <see cref="IEnumerable{T}"/>.</returns>
     public static Lens<T, TU> First<T, TU>(this Lens<T, TU[]> lens) => lens.Compose(First<TU>());
+
+    /// <summary>
+    /// Creates a <see cref="Lens"/> that maps the elements of the given sequence to another type.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+    /// <typeparam name="TU">The type of the elements in the resulting sequence.</typeparam>
+    /// <param name="selectorLens">The lens that maps a <typeparamref name="T"/> to a <typeparamref name="TU"/>.</param>
+    /// <returns>A <see cref="Lens{T,TU}"/>.</returns>
+    public static Lens<T[], TU[]> Select<T, TU>(Lens<T, TU> selectorLens) => new(array => array.Select(selectorLens.Get).ToArray());
 }
