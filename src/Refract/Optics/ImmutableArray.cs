5af326e32f07fcbd143ac5312194327bd4e82ee6
Wouter Naessens <wouter.naessens@sterima.be>
2023-08-25 10:58:05 +0200

    Add some optics for ImmutableArray and ImmutableDictionary

---
+++ src/Refract/Optics/ImmutableArray.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+
+namespace Refract.Optics; 
+
+/// <summary>
+/// Provides a number of optics for <see cref="ImmutableArray{T}"/>.
+/// </summary>
+public static class ImmutableArray {
+    /// <summary>
+    /// <see cref="Lens{T,TU}"/> that gets or sets a value in a <see cref="ImmutableArray{T}"/> at the given <paramref name="index"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the array elements.</typeparam>
+    /// <param name="index">The index to focus on.</param>
+    /// <returns>A <see cref="Lens{T,TU}"/>.</returns>
+    public static Lens<ImmutableArray<T>, T> ElementAt<T>(int index) =>
+        new(
+            array => array[index],
+            (value, array) => array.SetItem(index, value)
+        );
+}
